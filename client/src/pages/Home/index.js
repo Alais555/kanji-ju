9a2a04b39236d9c464f22fb7226143259a16a577
Aaron Nelson <axnelly55@gmail.com>
2020-08-24 14:07:21 -0700

    search page commits

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,29 +1,19 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import logo from "./logo.svg";
 import "./home.css";
-import { useAuth } from "../../utils/auth";
+import "./background1.png"
+
 
 function Home() {
-  const { user, logout } = useAuth();
-  const history = useHistory();
-
-  const goToEditProfile = () => history.push("/profile");
-
   return (
-    <div className="Home">
-      <div className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <h2>Welcome {user.email}</h2>
-      </div>
-      <p className="Home-intro">
-        <button onClick={goToEditProfile}>Go to Profile</button>
-        <button style={{ marginLeft: "1em" }} onClick={() => logout()}>
-          Logout
-        </button>
-      </p>
+    <div className="container align-center animebackground">
+      <div className="jumbotron">
+      <header className="name">KANJI-JU</header>
+    <h1 className="why">Why Kanji-Ju?</h1>
+      <p className="description"> The limits one may face as they are eagerly taking a dip into the Anime world are endless when it comes to not knowing where to start their search. You are stuck between netflix and hulu that barely cover the basics of the anime world to other sites charging an arm and leg for you to be able to access quality material. Where Kanji-JU  comes in, is that  it  takes away the need to go pay expensive monthly subscription fees in order to access anime sites. If users do have to pay for our site, it would not exceed $10 as most cost around $25. Our application also gives users access to hundreds of shows that are guaranteed to satisfy all their anime desires. Whether you are new to the world of anime or experienced,  our website gives users that sign up with us the option to save their selections and access them at a later time, all at little to no cost.
+</p>
     </div>
-  );
+    </div>
+  )
 }
 
 export default Home;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Search.css"
 import picture from "./Awesome-Anime-Background.jpg"
 
@@ -20,33 +20,44 @@ import picture from "./Awesome-Anime-Background.jpg"
 export default class Search extends Component {
   constructor(props) {
     super(props)
-    this.state= {
+    this.state = {
       userInput: ""
     }
   }
-  
+
+
+
   handleInputChange = (e) => {
-    this.setState({userInput: e.target.value})
+    this.setState({ userInput: e.target.value })
     console.log(e.target.value)
   }
   handleSearch = () => {
-    fetch("https://api.jikan.moe/v3").then(response => {
-    console.log(response)
-    }
-    )
-    }
+    fetch(`https://api.jikan.moe/v3/search/anime?q=${this.state.userInput}`)
+      // `${this.state.userInput}`
+      .then(response => response.json())
+      .then(data => console.log(data.results[0].image_url));
+  }
+
   render() {
     return (
       <div className="container align-center picture">
-      <div className="jumbotron">
-      <header className="name">KANJI-JU</header>
-    <input className="input" type="text" onChange={this.handleInputChange} />
-    <button className="anime-search" onClick={this.handleSearch}>Search Anime Collections</button>
-    <h1 className="search">Which Show Are You Interested In?</h1>
+        <div className="jumbotron">
+          <header className="name">KANJI-JU</header>
 
+          <button className="anime-search" onClick={this.handleSearch}>Search Anime Collections</button>
+          <input className="input" type="text" onChange={this.handleInputChange} />
 
-    </div>
-    </div>
+          {/* <h1 className="search">Which Show Are You Interested In?</h1> */}
+          <div className="topage">
+            <h1> Aaron</h1>
+            <img className="fit-picture"
+              src="https://via.placeholder.com/150"
+              alt="placeholder photo"></img>
+          </div>
+        </div>
+
+      </div>
+      
     )
   }
 }
